7d616034f8e1edee8c3a91581334c355effe6482
tonyiovino <antonioiovino2004tony@gmail.com>
2020-06-11 04:05:05 +0200

    tonyiovino

---
+++ tavola.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main(){
+
+    int num = 0;
+
+    printf("Tavola Pitagorica:\n");
+
+    num = 1;
+    while(num <= 10){
+        printf("%d    ", num);
+
+        num++;
+    }
+
+    printf("\n");
+
+    num = 2;
+    while(num <= 20){
+        printf("%d    ", num);
+
+        num += 2;
+    }
+
+    printf("\n");
+
+    num = 3;
+    while(num <= 30){
+        printf("%d    ", num);
+
+        num += 3;
+    }
+
+    printf("\n");
+
+    return 0;
+}
