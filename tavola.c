b5037f9966ee9930a167c756d52f610c5af64e5e
tonyiovino <antonioiovino2004tony@gmail.com>
2020-06-15 16:54:35 +0000

    Tabelline

--- tavola.c
+++ tavola.c
@@ -8,8 +8,7 @@ int main(){
 
     num = 1;
     while(num <= 10){
-        printf("%d     ", num);
-
+        printf("%-5d", num);
         num++;
     }
 
@@ -17,7 +16,7 @@ int main(){
 
     num = 2;
     while(num <= 20){
-        printf("%d     ", num);
+        printf("%-5d", num);
 
         num += 2;
     }
@@ -26,9 +25,72 @@ int main(){
 
     num = 3;
     while(num <= 30){
-        printf("%d     ", num);
+        printf("%-5d", num);
 
         num += 3;
+    }
+
+    printf("\n");
+
+    num = 4;
+    while(num <= 40){
+        printf("%-5d", num);
+
+        num += 4;
+    }
+
+    printf("\n");
+
+    num = 5;
+    while(num <= 50){
+        printf("%-5d", num);
+
+        num += 5;
+    }
+
+    printf("\n");
+
+    num = 6;
+    while(num <= 60){
+        printf("%-5d", num);
+
+        num += 6;
+    }
+
+    printf("\n");
+
+    num = 7;
+    while(num <= 70){
+        printf("%-5d", num);
+
+        num += 7;
+    }
+
+    printf("\n");
+
+    num = 8;
+    while(num <= 80){
+        printf("%-5d", num);
+
+        num += 8;
+    }
+
+    printf("\n");
+
+    num = 9;
+    while(num <= 90){
+        printf("%-5d", num);
+
+        num += 9;
+    }
+
+    printf("\n");
+
+    num = 10;
+    while(num <= 100){
+        printf("%-5d", num);
+
+        num += 10;
     }
 
     printf("\n");
