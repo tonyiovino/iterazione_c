afe934e30358865c9bfd6939462178dfde5013e7
tonyiovino <antonioiovino2004tony@gmail.com>
2020-06-16 00:15:39 +0000

    Maggiore tra 10

---
+++ max.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+    int numero;
+    int maggiore;
+    int count;
+    
+    printf("Inserisci 10 interi per determinare il maggiore.\n");
+
+    /*Prende 10 numeri e li confronta*/
+
+    printf("\nIl numero maggiore è: %d\n", maggiore);
+
+    return 0;
+}
