cc0adfc59df63f7e9497e4473891fc68a2aef0e9
tonyiovino <antonioiovino2004tony@gmail.com>
2020-07-17 19:34:17 +0200

    Quadrato Vuoto

--- quadrato_vuoto.c
+++ quadrato_vuoto.c
@@ -6,42 +6,40 @@ int main(void)
   int character = '$'; /*Carattere Speciale*/
   int larghezza;
   int altezza = 0;
-  int count = 0, count_2 = 0;
   int spazi;
 
   do {
     printf("Inserisci il lato (min 1, ma 20): ");
     scanf("%d", &lato);
 
-    if(lato < 1 || lato > 20){
+    if (lato < 1 || lato > 20){
       printf("Valore fuori dai limiti!\n\n");
     }
 
   } while (lato < 1 || lato > 20);
 
-  spazi = lato - 2;
+  spazi = lato - 1;
 
-  while(altezza < lato){
+  altezza = 0;
+  while (altezza < lato) {
+    putchar(character);
 
-    larghezza = 0;
-    while(larghezza < lato){
-      putchar(character);
+    larghezza = 1;
+    while (larghezza < spazi){
+
+      if (altezza <= spazi) {
+        putchar(character);
+      }
+
+      else {
+        putchar(' ');
+      }
 
       larghezza++;
-
-      putchar('\n');
-      putchar(character);
-
-      if(count == 1){
-        while(spazi == count_2){
-          putchar(' ');
-
-          count_2++;
-        }
-      }
     }
+    putchar(character);
+    putchar('\n');
 
-    count++;
     altezza++;
   }
 
