0381cc505e5d10432ddf16d6cfc525a9415b9465
tonyiovino <antonioiovino2004tony@gmail.com>
2020-08-12 18:46:22 +0200

    Ordine delle variabili

--- count_cifra.c
+++ count_cifra.c
@@ -3,7 +3,8 @@
 int main(void)
 {
   int num;
-  int count = 0;
+  int cifra;
+  int sette = 0;;
 
   do {
     printf("Inserisca un intero positivo: ");
@@ -14,6 +15,16 @@ int main(void)
     }
 
   } while (num < 0);
+
+  while (num > 0){
+    cifra = num % 10;
+    num /= 10;
+    if (cifra == 7){
+      sette++;
+    }
+  }
+
+  printf("%d\n", sette);
 
   return 0;
 }
