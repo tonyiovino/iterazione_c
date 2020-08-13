3529a2b4e54c27854f971d19a2cf482e956147f4
tonyiovino <antonioiovino2004tony@gmail.com>
2020-08-14 00:40:40 +0200

    Figure

---
+++ figure.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+  int a/*, b, c, d*/;
+
+  /*(a, b, c, d)s = (a, b, c, d) + "s"pace */
+  int as/*, bs, cs, ds*/;
+
+  printf("Figura A:\n");
+  for (a = 1; a <= 10; a += 1) {
+    putchar('*');
+    putchar('\n');
+  }
+  return 0;
+}
