d75df41575fe960c4242b9b30658f6840cde74f8
tonyiovino <antonioiovino2004tony@gmail.com>
2020-08-14 01:45:43 +0200

    Figure

--- figure.c
+++ figure.c
@@ -2,15 +2,20 @@
 
 int main()
 {
-  int a/*, b, c, d*/;
+  int a, /*b, c, d*/;
 
   /*(a, b, c, d)s = (a, b, c, d) + "s"pace */
-  int as/*, bs, cs, ds*/;
+  int as, bs/*, cs, ds*/;
 
-  printf("Figura A:\n");
-  for (a = 1; a <= 10; a += 1) {
-    putchar('*');
+  printf("Figura A:\n\n");
+  for (as = 1; as <= 10; as += 1) {
+    for (a = 1; a <= as; a += 1) {
+      putchar('*');
+    }
     putchar('\n');
   }
+  putchar('\n');
+  putchar('\n');
+
   return 0;
 }
