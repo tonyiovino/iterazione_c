8051207390d7fe13585f3d14cddb238f23db8303
tonyiovino <antonioiovino2004tony@gmail.com>
2020-08-14 00:16:43 +0200

    Fattoriale 2.0

--- fattoriale.c
+++ fattoriale.c
@@ -4,40 +4,16 @@ int main()
 {
   int n;
   int fattoriale = 1;
+  int count = 1;
 
-  /*Fattoriale 1*/
-  for (n = 1; n <= 1; n += 1) {
+  for (n = 1; n <= count; n += 1) {
     fattoriale *= n;
+    printf("%d! = %d\n", n, fattoriale);
+    count++;
+    if (count > 5) {
+        count = 0;
+    }
   }
-  printf("%d\n", fattoriale);
-
-  /*Fattoriale 2*/
-  fattoriale = 1;
-  for (n = 1; n <= 2; n += 1) {
-    fattoriale *= n;
-  }
-  printf("%d\n", fattoriale);
-
-  /*Fattoriale 3*/
-  fattoriale = 1;
-    for (n = 1; n <= 3; n += 1) {
-    fattoriale *= n;
-  }
-  printf("%d\n", fattoriale);
-
-  /*Fattoriale 4*/
-  fattoriale = 1;
-    for (n = 1; n <= 4; n += 1) {
-    fattoriale *= n;
-  }
-  printf("%d\n", fattoriale);
-
-  /*Fattoriale 5*/
-  fattoriale = 1;
-    for (n = 1; n <= 5; n += 1) {
-    fattoriale *= n;
-  }
-  printf("%d\n", fattoriale);
 
   return 0;
 }
