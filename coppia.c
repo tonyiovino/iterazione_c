593db557175729fb4ced2003d58a46148020c040
tonyiovino <antonioiovino2004tony@gmail.com>
2021-01-04 22:14:21 +0100

    Clean Fix

--- coppia.c
+++ coppia.c
@@ -2,24 +2,21 @@
 
 int main(void)
 {
-  /* per un singolo carattere
-   * meglio usare il tipo 'int'
-   */
-  int c1, c2, c3;
+    int c1, c2, c3;
 
-  printf("Inserisci primo carattere: ");
-  c1 = getchar();
+    printf("Inserisci primo carattere: ");
+    c1 = getchar();
 
-  printf("\nInserisci secondo carattere: ");
-  c2 = getchar();
+    printf("\nInserisci secondo carattere: ");
+    c2 = getchar();
 
-  printf("\nInserisci terzo carattere: ");
-  c3 = getchar();
+    printf("\nInserisci terzo carattere: ");
+    c3 = getchar();
 
-  printf("\n\nHai inserito i caratteri:\n");
-  printf("c1: %c\n", c1);
-  printf("c2: %c\n", c2);
-  printf("c3: %c\n", c3);
+    printf("\n\nHai inserito i caratteri:\n");
+    printf("c1: %c\n", c1);
+    printf("c2: %c\n", c2);
+    printf("c3: %c\n", c3);
 
-  return 0;
+    return 0;
 }
