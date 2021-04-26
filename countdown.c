712359960cd2f8b631227582c35486951ea6982b
tonyiovino <antonioiovino2004tony@gmail.com>
2021-04-26 13:31:47 +0200

    Clean

--- countdown.c
+++ countdown.c
@@ -3,20 +3,19 @@
 
 int main()
 {
+    int contatore;
+
     struct timespec intervallo;
     intervallo.tv_sec = 1;
     intervallo.tv_nsec = 0;
-    int contatore;
 
     printf("Conto alla rovescia:\n\n");
     printf("Da che numero devo partire? ");
     scanf("%d",&contatore);
     
-    while(contatore > 0){
-        printf("%d\n", contatore);
-        nanosleep(&intervallo, NULL);
-        contatore--;
-    }
+    printf("%d\n", contatore);
+    nanosleep(&intervallo, NULL);
+    contatore--;
 
     printf("Lanciato\n");
 
